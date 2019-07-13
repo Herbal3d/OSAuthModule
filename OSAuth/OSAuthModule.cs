554cabb25da30a59ac230d800bb1bb8357732d4c
Robert Adams <misterblue@misterblue.com>
2019-07-13 12:21:48 -0700

    Initial structure for region module.

---
+++ OSAuth/OSAuthModule.cs
@@ -0,0 +1,81 @@
+// Copyright 2019 Robert Adams
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+
+using System;
+using System.Reflection;
+using Mono.Addins;
+
+using OpenSim.Framework;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+
+using org.herbal3d.cs.CommonEntitiesUtil;
+
+using log4net;
+using Nini.Config;
+
+namespace org.herbal3d.OSAuth {
+
+    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "OSAuth")]
+    public class OSAuthModule : INonSharedRegionModule {
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly String _logHeader = "[OSAuth]";
+
+        private Scene _scene;
+
+        private bool _enabled = false;
+        private IConfig _params;
+
+        // IRegionModuleBase.Name
+        public string Name { get { return "OSAuthModule"; } }
+
+        // IRegionModuleBase.ReplaceableInterface
+        // This module has nothing to do with replaceable interfaces.
+        public Type ReplaceableInterface { get { return null; } }
+
+        // IRegionModuleBase.Initialize
+        public void Initialise(IConfigSource pConfig) {
+            _params = pConfig.Configs["OSAuth"];
+            _enabled = _params.GetBoolean("Enabled", false);
+            if (_enabled) {
+                _log.InfoFormat("{0} Enabled", _logHeader);
+            }
+        }
+        //
+        // IRegionModuleBase.Close
+        public void Close() {
+        }
+
+        // IRegionModuleBase.AddRegion
+        // Called once for the region we're managing.
+        public void AddRegion(Scene pScene) {
+            // Remember all the loaded scenes
+            _scene = pScene;
+        }
+
+        // IRegionModuleBase.RemoveRegion
+        public void RemoveRegion(Scene pScene) {
+            if (_scene != null) {
+                Close();
+                _scene = null;
+            }
+        }
+
+        // IRegionModuleBase.RegionLoaded
+        // Called once for each region loaded after all other regions have been loaded.
+        public void RegionLoaded(Scene scene) {
+            if (_enabled) {
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OSAuthModule
-OpenSimulator module for fetching JWT authentication token. Provides GRPC interfaces for token fetching, verification, and refresh.
+OpenSimulator module for fetching JWT authentication token.
+Provides a REST interface through the normal region HTTP connection to accept
+login credentials and return authorization tokens for later accesses.
+Also includes the routines for JWT token validation.
